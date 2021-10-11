9f1f8e05497dbee135ff0006a44788f4f181b82c
Andi Alfian Bahtiar <55624363+anddfian@users.noreply.github.com>
2021-10-11 22:18:56 +0800

    Update posttest1.py

--- posttest1.py
+++ posttest1.py
@@ -1,5 +1,7 @@
 import os
 
+kelompok = []
+
 def clear_screen():
 	os.system("cls" if os.name == "nt" else "clear")
 
@@ -9,22 +11,48 @@ def back_to_menu():
 
 def show_menu():
 	clear_screen()
-	kelompok = []
-	print("============================================")
-	print("| PROGRAM KLASIFIKASI ZONA RISIKO COVID-19 |")
-	print("============================================")
-	print("|                   OLEH                   |")
-	print("============================================")
-	print("|     ANDI ALFIAN BAHTIAR (2009106002)     |")
-	print("|            INFORMATIKA A 2020            |")
-	print("|              FAKULTAS TEKNIK             |")
-	print("|          UNIVERSITAS MULAWARMAN          |")
-	print("============================================")
-	objek = int(input("| Masukkan Jumlah Kab/Kota : "))
+	print("============================================================================")
+	print("|                 PROGRAM KLASIFIKASI ZONA RISIKO COVID-19                 |")
+	print("============================================================================")
+	print("|                                   OLEH                                   |")
+	print("============================================================================")
+	print("|                     ANDI ALFIAN BAHTIAR (2009106002)                     |")
+	print("|                            INFORMATIKA A 2020                            |")
+	print("|                              FAKULTAS TEKNIK                             |")
+	print("|                          UNIVERSITAS MULAWARMAN                          |")
+	print("============================================================================")
+	print("| [1] Klasifikasi Zona Risiko COVID-19                                     |")
+	print("| [2] Keluar                                                               |")
+	print("============================================================================")
+	try:
+		try:
+			selected_menu = int(input("| Pilih menu> "))
+			if selected_menu == 1:
+				klasifikasi_covid()
+			elif selected_menu == 0:
+				exit()
+			else:
+				print("============================================================================")
+				print("| Error: Kamu memilih menu yang salah!                                     |")
+				print("============================================================================")
+				back_to_menu()
+		except ValueError:
+			print("============================================================================")
+			print("| Error: Kamu memilih menu yang salah!                                     |")
+			print("============================================================================")
+			back_to_menu()
+	except KeyboardInterrupt:
+		exit()
+
+def klasifikasi_covid():
 	clear_screen()
-	print("============================================")
-	print("| PROGRAM KLASIFIKASI ZONA RISIKO COVID-19 |")
-	print("============================================")
+	global kelompok
+	kelompok.clear()
+	print("============================================================================")
+	print("|                 PROGRAM KLASIFIKASI ZONA RISIKO COVID-19                 |")
+	print("============================================================================")
+	objek = int(input("| Masukkan Jumlah Kab/Kota : "))
+	print("============================================================================")
 	while objek > 0:
 		nama = input("| Masukkan Nama Kota/Kab               : ")
 		positif = int(input("| Masukkan Jumlah Pasien Terkonfirmasi : "))
@@ -41,12 +69,16 @@ def show_menu():
 		elif kasus_aktif < 1:
 			risiko = "Zona Tidak Terdampak"
 		kelompok.append([nama, positif, sembuh, meninggal, kasus_aktif, risiko])
-		print("============================================")
+		print("============================================================================")
 		objek -= 1
+	input("\nTekan 'Enter' untuk melanjutkan...")
+	show_klasifikasi()
+
+def show_klasifikasi():
 	clear_screen()
-	print("============================================")
-	print("| PROGRAM KLASIFIKASI ZONA RISIKO COVID-19 |")
-	print("============================================")
+	print("============================================================================")
+	print("|                 PROGRAM KLASIFIKASI ZONA RISIKO COVID-19                 |")
+	print("============================================================================")
 	for i in range(len(kelompok)):
 		print("| Kota/Kab ke-%d" % (i + 1))
 		print("| Nama Kota/Kab               :", kelompok[i][0])
@@ -55,14 +87,14 @@ def show_menu():
 		print("| Jumlah Pasien Meninggal     :", kelompok[i][3])
 		print("| Jumlah Kasus Aktif          :", kelompok[i][4])
 		print("| Status                      :", kelompok[i][5])
-		print("============================================")
+		print("============================================================================")
 	back_to_menu()
 
 if __name__ == "__main__":
 	try:
 		show_menu()
 	except KeyboardInterrupt:
-		print("============================================================================")
+		print("\n============================================================================")
 		print("| Terima kasih telah menggunakan Aplikasi Klasifikasi Zona Risiko COVID-19 |")
 		print("============================================================================")
 		exit()
